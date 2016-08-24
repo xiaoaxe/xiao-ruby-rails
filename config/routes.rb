91f2e384c1552afd5fdf766e2a43ec23dcca4fc2
baoqiang <baoqiang@uzoo.cn>
2016-08-24 13:07:33 +0800

    change index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # get 'home/index' => "home"
-  get 'home/index'
+  # get 'home/index' => "home#index"
+  root :to => 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
