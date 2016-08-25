b6dd65bdb731df5bef8e4b44d26a9b02327fa807
baoqiang <baoqiang@uzoo.cn>
2016-08-25 14:21:02 +0800

    add post mvc

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @first_content = 'This is some sample text for our awesome new Ruby blog'
+    @second_content = 'This is more sample text for our awesome new Ruby blog'
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
