43aa551b59fedfd238ca6ed8c157292e61e4441f
baoqiang <baoqiang@uzoo.cn>
2016-08-26 01:04:56 +0800

    learn up to chapter 23, it is a little hard for a newer like me

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,19 +6,32 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
+    @category=Category.all
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success]='Your post is saved!'
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update_attributes(params[:post])
+      redirect_to post_path, :notice => 'Your post has been updated'
+    else
+      render edit
+    end
   end
 
   def show
@@ -26,6 +39,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path :notice => 'Your post has been deleted'
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id, :author_id)
+  end
+
 end
