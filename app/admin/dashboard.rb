b7fe38a7f5efc009507b1a6ff745f7fd5217baf3
baoqiang <baoqiang@uzoo.cn>
2016-08-28 19:44:46 +0800

    css and html

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,14 +1,14 @@
-ActiveAdmin.register_page "Dashboard" do
-
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+ActiveAdmin.register_page 'Dashboard'  do
+  # columns 'Recent Posts ' do
+  #   table_for Post.order('id desc').limit(10) do
+  #     column :id
+  #     column 'Post Title', :title do |post|
+  #       link_to post.name, [admin,post]
+  #     end
+  #     column :category,:sortable => :category
+  #     column :create_at
+  #   end
+  #   strong {link_to 'Show All Posts',:posts}
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -22,7 +22,7 @@ ActiveAdmin.register_page "Dashboard" do
     #       end
     #     end
     #   end
-
+    #
     #   column do
     #     panel "Info" do
     #       para "Welcome to ActiveAdmin."
@@ -30,4 +30,3 @@ ActiveAdmin.register_page "Dashboard" do
     #   end
     # end
   end # content
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/resources'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :categories
@@ -14,6 +20,9 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   resources :posts
   resources :categories
+  get 'about', :to => 'pages#about'
+  get 'contact', :to => 'pages#contact'
+  get 'resources', :to => 'pages#resources'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
